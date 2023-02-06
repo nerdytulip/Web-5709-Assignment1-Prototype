7afd5393419629d7967ce8fd699d6c08e9f5a8ca
Parul Raich <pr678267@dal.ca>
2023-02-06 15:04:45 -0400

    updated logo

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
-import logo from './logo.svg';
-import './App.css';
-import Bookmark from './components/Bookmark';
+import logo from "./logo.svg";
+import "./App.css";
+import Bookmark from "./components/Bookmark";
 
-import { Navbar,Container,Nav,Form,Button} from 'react-bootstrap';
+import { Navbar, Container, Nav, Form, Button } from "react-bootstrap";
 function App() {
   return (
     <div className="App">
@@ -10,37 +10,36 @@ function App() {
         <Container>
           <Navbar.Brand href="#home">
             <img
-              src={logo}
-              width="30"
-              height="30"
+              src="cookwithdal_logo.jpg"
+              width="60"
+              height="50"
               className="d-inline-block align-top"
               alt="React Bootstrap logo"
             />
-            </Navbar.Brand>
-            {/* className="me-auto" */}
+          </Navbar.Brand>
+          {/* className="me-auto" */}
           <Nav className="justify-content-end">
             <Nav.Link href="#home">Feed</Nav.Link>
             <Nav.Link href="#features">Planner</Nav.Link>
             <Nav.Link href="#pricing">Shopping List</Nav.Link>
             <Form className="d-flex">
-            <Form.Control
-              type="search"
-              placeholder="Search"
-              className="me-2"
-              aria-label="Search"
-            />
-            <Button variant="outline-light">Search</Button>
-          </Form>
-          <Nav.Link href="#profile">Profile</Nav.Link>
+              <Form.Control
+                type="search"
+                placeholder="Search"
+                className="me-2"
+                aria-label="Search"
+              />
+              <Button variant="outline-light">Search</Button>
+            </Form>
+            <Nav.Link href="#profile">Profile</Nav.Link>
           </Nav>
         </Container>
       </Navbar>
 
-      <br/><br/>
-      <Bookmark bookmarkState={false}
-        bookmarkImageSrc={"unselected.jpg"}
-      />
-      
+      <br />
+      <br />
+      <Bookmark bookmarkState={false} bookmarkImageSrc={"unselected.jpg"} />
+
       {/* <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
