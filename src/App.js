52978ddc67830a7223b4061ae31c08e59527b296
Parul Raich <pr678267@dal.ca>
2023-02-06 13:39:25 -0400

    initial commit

---
+++ src/App.js
@@ -0,0 +1,62 @@
+import logo from './logo.svg';
+import './App.css';
+import Bookmark from './components/Bookmark';
+
+import { Navbar,Container,Nav,Form,Button} from 'react-bootstrap';
+function App() {
+  return (
+    <div className="App">
+      <Navbar bg="dark" variant="dark" expand="lg">
+        <Container>
+          <Navbar.Brand href="#home">
+            <img
+              src={logo}
+              width="30"
+              height="30"
+              className="d-inline-block align-top"
+              alt="React Bootstrap logo"
+            />
+            </Navbar.Brand>
+            {/* className="me-auto" */}
+          <Nav className="justify-content-end">
+            <Nav.Link href="#home">Feed</Nav.Link>
+            <Nav.Link href="#features">Planner</Nav.Link>
+            <Nav.Link href="#pricing">Shopping List</Nav.Link>
+            <Form className="d-flex">
+            <Form.Control
+              type="search"
+              placeholder="Search"
+              className="me-2"
+              aria-label="Search"
+            />
+            <Button variant="outline-light">Search</Button>
+          </Form>
+          <Nav.Link href="#profile">Profile</Nav.Link>
+          </Nav>
+        </Container>
+      </Navbar>
+
+      <br/><br/>
+      <Bookmark bookmarkState={false}
+        bookmarkImageSrc={"unselected.jpg"}
+      />
+      
+      {/* <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header> */}
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Bookmark.js
@@ -0,0 +1,103 @@
+import "./Bookmark.css";
+import "bootstrap/dist/css/bootstrap.css";
+import { useState } from "react";
+import { Col, Container, Row } from "react-bootstrap";
+import { Typography } from "@mui/material";
+import { Box } from "@mui/system";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+
+function Bookmark(props) {
+  const [bookmarkState, setBookmarkState] = useState(props.bookmarkState);
+  const [bookmarkImageSrc, setbookmarkImageSrc] = useState(
+    props.bookmarkImageSrc
+  );
+
+  const onClickBookmarkHandler = () => {
+    //negatation
+    const mybool = !bookmarkState;
+    console.log(mybool);
+    setBookmarkState(mybool);
+    if (mybool) {
+      console.log("inside bookmark state true");
+      setbookmarkImageSrc("selected.jpg");
+      toast.success("Recipe Saved to Profile !", {
+        position: toast.POSITION.BOTTOM_RIGHT,
+      });
+    } else {
+      console.log("inside bookmark state false");
+      setbookmarkImageSrc("unselected.jpg");
+    }
+  };
+
+  return (
+    <div>
+      <Container>
+        <Row>
+          <Col xs={10} style={{ backgroundColor: "#dbf0f0", height: "70vh" }}>
+            <Row>
+              <Typography variant="h3" align="center">
+                <b>Quickie Recipe</b>
+              </Typography>
+            </Row>
+            <br></br>
+            {/* <Row>
+              <div>
+                <img src="recipe.jpg" width="200" height="200" />
+              </div>
+            </Row> */}
+            <br></br>
+            <Row>
+              <Col>
+                <Box
+                  component="img"
+                  sx={{
+                    height: 350,
+                    width: 350,
+                    maxHeight: { xs: 350, md: 167 },
+                    maxWidth: { xs: 350, md: 250 },
+                    alignItems: "center",
+                  }}
+                  alt="Recipe"
+                  src="recipe.jpg"
+                  m="auto"
+                />
+              </Col>
+              <Col>
+                <Typography variant="h5" align="left">
+                  Description:
+                </Typography>
+                <Typography variant="body1" align="justify">
+                  Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed
+                  do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+                  Ut enim ad minim veniam, quis nostrud exercitation ullamco
+                  laboris nisi ut aliquip ex ea commodo consequat. Duis aute
+                  irure dolor in reprehenderit in voluptate velit esse cillum
+                  dolore eu fugiat nulla pariatur. Excepteur sint occaecat
+                  cupidatat non proident, sunt in culpa qui officia deserunt
+                  mollit anim id est laborum.
+                </Typography>
+              </Col>
+            </Row>
+          </Col>
+          <Col xs={2} style={{ backgroundColor: "#f0f0db", height: "70vh" }}>
+            <br></br>
+            <div>
+              {/* <img src={bookmarkImageSrc}></img> <button onClick={onClickBookmarkHandler}>Click Me!</button> */}
+              <img
+                src={bookmarkImageSrc}
+                width="40"
+                height="40"
+                alt="Bookmark"
+                onClick={onClickBookmarkHandler}
+              />
+            </div>
+          </Col>
+        </Row>
+      </Container>
+      <ToastContainer autoClose={5000} />
+    </div>
+  );
+}
+
+export default Bookmark;
